e0d0752b6615053817835cd9cac1e605ee0c53f8
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-25 21:16:54 +0530

    integrated mongodb

--- api/index.js
+++ api/index.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const cors = require('cors')
+const { default: mongoose } = require("mongoose")
+require('dotenv').config()
 const app = express()
 
+app.use(express.json())
 app.use(cors({
   credentials: true,
   origin: 'http://192.168.1.8:5173'
 }))
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res) => {
   res.json('test successful!')
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({name, email, password})
 })
 
 app.listen(4000)
