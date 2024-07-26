20da29a04380b736dc3a4baa7b3649e1685c87f1
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-26 21:26:57 +0530

    changed origin from 192.. to 127.0.0.1/5173

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const jwtSecret = 'hwsrdfserwwdf36g'
 app.use(express.json())
 app.use(cors({
   credentials: true,
-  origin: /* 'http://192.168.1.8:5173' */ 'http://localhost:5173'
+  origin: 'http://127.0.0.1:5173'
 }))
 
 mongoose.connect(process.env.MONGO_URL)
