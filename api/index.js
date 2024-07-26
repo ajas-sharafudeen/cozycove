e9b4ecb554bfcff9305af40fcfd6956b12ca0db3
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-26 18:10:00 +0530

    integrated jwt on index.js

--- api/index.js
+++ api/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('./models/User.js');
 require('dotenv').config()
 const app = express()
 
 const bcryptSalt = bcrypt.genSaltSync(10)
+const jwtSecret = 'hwsrdfserwwdf36g'
 
 app.use(express.json())
 app.use(cors({
   credentials: true,
-  origin: 'http://192.168.1.8:5173'
+  origin: /* 'http://192.168.1.8:5173' */ 'http://localhost:5173'
 }))
 
 mongoose.connect(process.env.MONGO_URL)
@@ -22,13 +24,34 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
-  const userDoc = await User.create({
-    name,
-    email,
-    password: bcrypt.hashSync(password, bcryptSalt),
-  })
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    })
+    res.json(userDoc)
+  } catch (e) {
+    res.status(422).json(e)
+  }
+})
 
-  res.json(userDoc)
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email })
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+    if (passOk) {
+      jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).json('password OK')
+      })
+    } else {
+      res.status(422).json('password not OK')
+    }
+  } else {
+    res.json('not found')
+  }
 })
 
 app.listen(4000)
