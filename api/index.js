88aca9db2f3d5ace56a632df2d624fa52030b3aa
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-27 14:41:57 +0530

    profile name refresh

--- api/index.js
+++ api/index.js
@@ -44,7 +44,10 @@ app.post('/login', async (req, res) => {
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password)
     if (passOk) {
-      jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+      jwt.sign({
+        email: userDoc.email,
+        id: userDoc._id,
+      }, jwtSecret, {}, (err, token) => {
         if (err) throw err;
         res.cookie('token', token).json(userDoc)
       })
@@ -59,14 +62,14 @@ app.post('/login', async (req, res) => {
 app.get('/profile', (req, res) => {
   const { token } = req.cookies
   if (token) {
-    jwt.verify(token, jwtSecret, {}, (err,user) => {
-      if(err) throw err
-      res.json(user)
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err
+      const { name, email, _id } = await User.findById(userData.id)
+      res.json({ name, email, _id })
     })
   } else {
     res.json(null)
   }
-  res.json({ token })
 })
 
 app.listen(4000)
