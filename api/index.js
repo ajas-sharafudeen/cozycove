f4958a9db8c4e0d01719a8d0cfbce62447e7ba7c
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-29 19:58:43 +0530

    uploading feature using multer added to the api

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const User = require('./models/User.js');
 const cookieParser = require('cookie-parser')
 const imageDownloader = require('image-downloader')
 const multer = require('multer')
+const fs = require('fs')
 
 require('dotenv').config()
 const app = express()
@@ -90,9 +91,18 @@ app.post('/upload-by-link', async (req, res) => {
   res.json(newName)
 })
 
-const photosMiddleware = multer({ dest: 'uploads' })
-app.post('/upload', (req, res) => {
-
+const photosMiddleware = multer({ dest: 'uploads/' })
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+  const uploadedFiles = []
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i]
+    const parts = originalname.split('.')
+    const ext = parts[parts.length - 1]
+    const newPath = path + '.' + ext
+    fs.renameSync(path, newPath)
+    uploadedFiles.push(newPath.replace('uploads/', ''))
+  }
+  res.json(uploadedFiles)
 })
 
 app.listen(4000)
