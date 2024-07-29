eff04fa9669f90a4ffa42ef20e7d98a4b12f62c3
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-29 19:58:06 +0530

    linked PhotoUploader component

--- client/src/pages/PlacesPage.tsx
+++ client/src/pages/PlacesPage.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
-import axios from "axios";
+import PhotosUploader from "../PhotosUploader";
 
 export default function PlacesPage() {
   const { action } = useParams()
   const [title, setTitle] = useState('')
   const [address, setAddress] = useState('')
   const [addedPhotos, setAddedPhotos] = useState([])
-  const [photoLink, setPhotoLink] = useState('')
   const [description, setDescription] = useState('')
   const [perks, setPerks] = useState([])
   const [extraInfo, setExtraInfo] = useState('')
@@ -40,34 +39,7 @@ export default function PlacesPage() {
       </>
     )
   }
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  async function addPhotoByLink(ev) {
-    ev.preventDefault()
-    const { data: filename } = await axios.post('/upload-by-link', { link: photoLink })
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    setAddedPhotos(prev => {
-      return [...prev, filename]
-    })
-    setPhotoLink('')
-  }
-  function uploadPhoto(ev) {
-    const files = ev.target.files
-    const data = new FormData()
-    data.set('photos', files)
-    axios.post('/upload', data, {
-      headers: { 'Context-type': 'multipart/form-data' }
-    }).then(response => {
-      const { data: filename } = response
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      setAddedPhotos(prev => {
-        return [...prev, filename]
-      })
-    })
 
-  }
   return (
     <div>
       {action != 'new' && (
@@ -89,27 +61,7 @@ export default function PlacesPage() {
             {preInput('Address', 'Address to this place')}
             <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
             {preInput('Photos', 'more = better')}
-            <div className="flex gap-2">
-              <input
-                value={photoLink}
-                onChange={ev => setPhotoLink(ev.target.value)}
-                type="text" placeholder={'Add using a link ...jpg'} />
-              <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-            </div>
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-3 lg:grid-cols-6">
-              {addedPhotos.length > 0 && addedPhotos.map(link => (
-                <div>
-                  <img className="rounded-2xl" src={'http://localhost:4000/uploads/' + link} alt="" />
-                </div>
-              ))}
-              <label className="cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input type="file" className="hidden" onChange={uploadPhoto} />
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
-                </svg>
-                Upload
-              </label>
-            </div>
+            <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
             {preInput('Description', 'description of the place')}
             <textarea value={description} onChange={ev => setDescription(ev.target.value)} className="" />
             {preInput('Perks', 'select all the perks of your place')}
