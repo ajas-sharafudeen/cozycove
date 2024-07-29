68a26ca363a28e6498a1252fc49d69bfee5b43b8
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-29 21:31:58 +0530

    removed useParams hook

--- client/src/pages/PlacesPage.tsx
+++ client/src/pages/PlacesPage.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AccountNav from "../AccountNav";
 
 export default function PlacesPage() {
@@ -6,6 +6,8 @@ export default function PlacesPage() {
     <div>
       <AccountNav />
       <div className="text-center">
+        list of all Places
+        <br />
         <Link className='inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full' to={'/account/places/new'}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
