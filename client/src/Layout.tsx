71cb945f1d11a3a1a809721f9cff277c1c9fa52d
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-07-31 21:16:28 +0530

    layout spacing is widened for comfortable viewing experience

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return(
-    <div className='p-4 flex flex-col min-h-screen'>
+    <div className='py-4 px-8 flex flex-col min-h-screen'>
       <Header />
       <Outlet />
     </div>
