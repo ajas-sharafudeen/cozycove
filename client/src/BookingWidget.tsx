e9dc45f9b87d7ab3b098f25f9ee512c7ff21a444
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-08-01 23:22:10 +0530

    added details and functionality for Booking Widget

--- client/src/BookingWidget.tsx
+++ client/src/BookingWidget.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react"
+import axios from "axios"
+import { differenceInCalendarDays } from "date-fns"
+import { useContext, useEffect, useState } from "react"
+import { Navigate } from "react-router"
+import { UserContext } from "./UserContext"
 
 export default function BookingWidget({ place }) {
   const [checkIn, setCheckIn] = useState('')
   const [checkOut, setCheckOut] = useState('')
   const [numberOfGuests, setNumberOfGuests] = useState(1)
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [redirect, setRedirect] = useState('')
+  const { user } = useContext(UserContext)
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name)
+    }
+  }, [user])
+
+
+  let numberOfNights = 0
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn))
+  }
+
+  async function bookThisPlace() {
+    const response = await axios.post('/bookings', {
+      checkIn, checkOut, numberOfGuests, name, phone,
+      place: place._id,
+      price: numberOfNights * place.price
+    })
+    const bookingId = response.data._id
+    setRedirect(`/account/bookings/${bookingId}`)
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />
+  }
+
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -31,9 +66,28 @@ export default function BookingWidget({ place }) {
               value={numberOfGuests}
               onChange={ev => setNumberOfGuests(ev.target.value)} />
           </div>
+          {numberOfNights > 0 && (
+            <div className="px-4 py-3 border-t">
+              <label>Your full name:</label>
+              <input type="text"
+                value={name}
+                onChange={ev => setName(ev.target.value)} />
+              <label>Phone number:</label>
+              <input type="tel"
+                value={phone}
+                onChange={ev => setPhone(ev.target.value)} />
+            </div>
+          )}
         </div>
       </div>
-      <button className="primary mt-4">Book this place</button>
+      <button onClick={bookThisPlace} className="primary mt-4">
+        Book this place
+        {numberOfNights > 0 && (
+          <>
+            <span> &#8377;{numberOfNights * place.price}</span>
+          </>
+        )}
+      </button>
     </div>
   )
 }
