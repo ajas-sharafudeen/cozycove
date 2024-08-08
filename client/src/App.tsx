796d9d5f9161d81be1bb5c927c0469d530573ea4
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-08-08 21:56:39 +0530

    log to check URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,7 @@ import BookingPage from './pages/BookingPage'
 // axios.defaults.baseURL = 'http://127.0.0.1:4000'
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
+console.log('API Base URL:', axios.defaults.baseURL);
 
 function App() {
   return (
