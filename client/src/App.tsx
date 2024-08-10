5dcac5a933b03c9e2e2d6aedd46ddb823e97d5ff
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-08-10 17:22:27 +0530

    env production typo fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,9 +13,8 @@ import BookingsPage from './pages/BookingsPage'
 import BookingPage from './pages/BookingPage'
 
 // axios.defaults.baseURL = 'http://127.0.0.1:4000'
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = import.meta.env.production.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
-console.log('API Base URL:', axios.defaults.baseURL);
 
 function App() {
   return (
